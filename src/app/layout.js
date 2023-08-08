3a4f67541ad6745d84fc8e78136fbe4331314f21
brunobarross <altamiro_bruno@yahoo.com.br>
2023-08-08 20:07:10 -0300

    ...

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,14 +28,14 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM}`} />
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`} />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', '${process.env.NEXT_PUBLIC_GTM}');
+          gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
         `}
       </Script>
       <body>
