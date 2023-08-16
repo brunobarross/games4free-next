065441cc7a86ae726ffc9b32db4456bd8250a44f
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2023-08-16 08:02:42 -0300

    ads terra

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,22 @@
-import Sidebar from './components/Sidebar';
-import './globals.css';
-import { Poppins } from '@next/font/google';
-import React from 'react';
-import SidebarProvider from './context/GlobalContext';
-import Script from 'next/script';
-import Head from 'next/head';
+import Sidebar from "./components/Sidebar";
+import "./globals.css";
+import { Poppins } from "@next/font/google";
+import React from "react";
+import SidebarProvider from "./context/GlobalContext";
+import Script from "next/script";
+import Head from "next/head";
 
 export const poppins = Poppins({
-  weight: ['300', '400', '500', '600', '700'],
+  weight: ["300", "400", "500", "600", "700"],
   preload: false,
-  display: 'swap',
+  display: "swap",
 });
 
 export const metadata = {
-  title: 'Games4Free | Jogos Steam, Epic Games, Playstation e Xbox Grátis',
+  title: "Games4Free | Jogos Steam, Epic Games, Playstation e Xbox Grátis",
   description:
-    'Os melhores jogos gratuitos da Steam, Epic, PS4 e Xbox estão aqui. Resgate as chaves por tempo limitado.',
-  keywords: 'games, jogos, gratis, free, dlcs, dlc, epic, steam, ubisoft',
+    "Os melhores jogos gratuitos da Steam, Epic, PS4 e Xbox estão aqui. Resgate as chaves por tempo limitado.",
+  keywords: "games, jogos, gratis, free, dlcs, dlc, epic, steam, ubisoft",
 };
 
 export default function RootLayout({ children }) {
@@ -30,8 +30,22 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.ico" />
         <title>{metadata.title}</title>
       </head>
-      <Script src='//pl20344809.highcpmrevenuegate.com/17/06/3a/17063ae5bbebe7bf8e887428f431181d.js'></Script>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM}`} />
+      <Script src="//pl20344809.highcpmrevenuegate.com/17/06/3a/17063ae5bbebe7bf8e887428f431181d.js"></Script>
+
+      <Script
+        src={`atOptions = {
+            'key' : 'bfecd93799d03ae0e3da3309d315194b',
+            'format' : 'iframe',
+            'height' : 60,
+            'width' : 468,
+            'params' : {}
+          };
+	      document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitablecreativeformat.com/bfecd93799d03ae0e3da3309d315194b/invoke.js"></scr' + 'ipt>');`}
+      />
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM}`}
+      />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
